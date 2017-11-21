a5e9dab4736ffcb2501b456e9141e915256cc580
kevin alviola <kevin@grc.digital>
2017-11-21 19:28:57 +0800

    update protecting modules

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,11 +2,13 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { DashBoardComponent } from './dashboard.component';
+import { AuthGuard } from '../shared/services';
 
 export const route: ModuleWithProviders = RouterModule.forChild([
   {
     path: 'dashboard',
-    component: DashBoardComponent
+    component: DashBoardComponent,
+    canActivate: [AuthGuard]
   }
 ]);
 
