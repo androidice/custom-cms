5ebef5646682eaf22c25d58646e81a3b1eef2002
kevin alviola <kevin@grc.digital>
2017-11-21 13:47:55 +0800

    update using shared component
    - define Home Moudle
    - using angular router with router outlet
    - including component and new module in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
+import { HomeModule } from './home/home.module';
+
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { FooterComponent, HeaderComponent} from './shared';
 
 import { RegisterService } from '../services/register.service';
 
@@ -19,18 +23,24 @@ export const firebaseConfig = {
   messagingSenderId: "842267089351"
 };
 
+export const rootRouting: ModuleWithProviders = RouterModule.forRoot([]);
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    HomeModule,
+    rootRouting
   ],
   providers: [
     RegisterService
